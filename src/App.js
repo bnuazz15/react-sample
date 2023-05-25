1848c40eb223862d217a5a845fc04fe4a4a0c8c8
bnuazz15 <98257599+bnuazz15@users.noreply.github.com>
2023-05-25 19:38:23 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React ver 2
+          Learn React ver 3
         </a>
       </header>
     </div>
